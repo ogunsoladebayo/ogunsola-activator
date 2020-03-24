18c14deb47766fec529b11f8ac0b12a425d84596
OGUNSOLA USMAN ADEBAYO <57511444+Ogunsola-activator@users.noreply.github.com>
2020-03-24 19:47:40 +0100

    Add files via upload

---
+++ ogunsola-activator.js
@@ -0,0 +1,16 @@
+var name = "Ogunsola Usman Adebayo";
+var courses = ["Python", "JavaScript", "C#", "PHP"];
+console.log("Name: Ogunsola Usman Adebayo " + "Courses: Python, JavaScript, C#, PHP");
+const data = [];
+
+for (let i=0; i<=200; i++) {
+
+  if (i%2===0) {
+
+    data.push(i);
+
+  }
+
+}
+
+console.log(data);
