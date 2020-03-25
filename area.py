c4e489d89b5f8c4623d1564624af67b68f07b371
OGUNSOLA USMAN ADEBAYO <57511444+Ogunsola-activator@users.noreply.github.com>
2020-03-25 14:23:41 +0100

    Update area.py

--- area.py
+++ area.py
@@ -1,6 +1,6 @@
 # Stores the input in the variable radius 
 radius = input("What is your radius? ")
 #converts the value in response to float
-r = float(response)
+r = float(radius)
 area = 3.14159 * r**2
 print("The area is ", area)
