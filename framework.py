fc60cb2d8d86affa26e869c0966f920dca619539
Ogunsola Usman Adebayo <ogunsoladebayo@gmail.com>
2020-04-10 19:17:54 +0100

    new changes

--- framework.py
+++ framework.py
@@ -5,22 +5,28 @@ each_user = {}
 counter = 0
 # this for loop allow collection of details multiple times
 for i in database:
+   # collect First Name, Last name and email address
    first_name = input("Please, enter your first name: ")
    last_name = input("Please, enter your last name: ")
    email = input("Please, enter your email address: ")
+   # updates counter value that will be used as ID for the user
+   counter = counter + 1
+   # stores all user details collected in a dictionary
+   each_user = {
+               "ID" : counter,
+               "First name" : first_name,
+               "last name" : last_name,
+               "email" : email}
+   # generate random password
    import string
    import random
    f = ''
    for i in range(5):
       f = f + random.choice(string.ascii_letters)
    suggest_password =(first_name[0:2] + last_name[-3:-1] + f)
+   # prints name and password generated
    print('Welcome ' + first_name + ', Your default password is ' + suggest_password)
-   counter = counter + 1
-   each_user = {
-               "ID" : counter,
-               "First name" : first_name,
-               "last name" : last_name,
-               "email" : email}
+   # Asks user if it wants to change the password
    password_choice = input("Do you wish to change this password? Type 'Yes' or 'No' only: ")
    password_use = True
    while password_use:
