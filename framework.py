d9a7d4bf590842f9edc16b8526c2b8df6bb27bd9
Ogunsola Usman Adebayo <ogunsoladebayo@gmail.com>
2020-04-10 18:12:06 +0100

    new changes

--- framework.py
+++ framework.py
@@ -1,11 +1,9 @@
-# from password import password
+# overall database variable
 database = [0]
-details = {}
+# individual user variable
+each_user = {}
 counter = 0
-new = True
 for i in database:
-   counter = counter + 1
-   details['ID'] = counter
    new_user = True
    while new_user:
       # index = 'user' + str(counter + 1)
@@ -24,15 +22,18 @@ for i in database:
       suggest_password =(f_name_spread[0] + f_name_spread[1] + l_name_spread[-2] + l_name_spread[-1] + f)
       print('Welcome ' + first_name + ', Your default password is ' + suggest_password)
       # password(first_name, last_name)
-      details = {"First name" : first_name,
+      counter = counter + 1
+      each_user = {
+                  "ID" : counter,
+                  "First name" : first_name,
                   "last name" : last_name,
                   "email" : email}
       password_choice = input("Do you wish to change this password? Type 'Yes' or 'No' only: ")
       password_use = True
       while password_use == True:
          if password_choice == "No":
-            details["password"] = suggest_password
-            print("Your details have been saved")
+            each_user["password"] = suggest_password
+            print("Your each_user have been saved")
             password_use = False
             new_user = False
          elif password_choice == "Yes":
@@ -43,16 +44,15 @@ for i in database:
                   print("Password is less than 7 characters")
                   # suggest_password = input("Enter a new password longer than or equal to 7 characters: ")
                else:
-                  details["password"] = suggest_password
+                  each_user["password"] = suggest_password
                   print("Your password has been updated")
                   enter_password = False
             password_use = False
-      database.append(details)
+      database.append(each_user)
       enter_new_user = input("Would you like to enter another user: ")
       if enter_new_user == "Yes":
          new_user = True
       elif enter_new_user == "No":
          new_user = False
-         new = False
          print(database)
    break
