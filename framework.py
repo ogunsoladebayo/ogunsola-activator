0b2b621c009d6db8a4d83f9f1e1c23b0fc14f4b5
Ogunsola Usman Adebayo <ogunsoladebayo@gmail.com>
2020-04-18 00:21:46 +0100

    new commit

--- framework.py
+++ framework.py
@@ -3,30 +3,29 @@ database = ["All user details: "]
 # individual user variable
 each_user = {}
 counter = 0
-# generate random password
+# function to generate random password
 def func_password(first_name, last_name):
    import string
    import random
    f = ''
    for i in range(5):
       f = f + random.choice(string.ascii_letters)
-   suggest_password =(first_name[0:2] + last_name[-2:] + f)
+   suggest_password =(first_name[:2] + last_name[-2:] + f)
    return suggest_password
 
 
 
-# this for loop allow collection of details multiple times
+# this loop continues to run until user does not wish to add new details
 for i in database:
+   # updates counter value that will be used as ID for the user
+   counter = counter + 1
+
    # collect First Name, Last name and email address
    first_name = input("Please, enter your first name: ")
    last_name = input("Please, enter your last name: ")
    email = input("Please, enter your email address: ")
    password = func_password(first_name, last_name)
 
-   # updates counter value that will be used as ID for the user
-   counter = counter + 1
-   # prints name and password generated               
-   print('Welcome ' + first_name + ', Your default password is ' + password)
    
    # stores all user details collected in a dictionary
    each_user = {
@@ -37,37 +36,44 @@ for i in database:
                "password" : password
                }
 
+   # prints name and password generated               
+   print('Welcome ' + first_name + ', Your default password is ' + password)
 
    
    # Asks user if they want to change the password
-   password_choice = input("Do you wish to change this password? Type 'Yes' or 'No' only: ")
    password_use = True
    
-   
-   while password_use:
-      if password_choice.lower() == "no":
-         # save password if user does not wish to change generated password
-         each_user["password"] = password
-         print("Your details have been saved")
-         password_use = False
-
-
-      elif password_choice.lower() == "yes":
-         # setting new password
-         enter_password = True
-
-         while enter_password:
-            password = input("Enter a new password not less than 7 characters: ")
-            # when password is less than 7 characters
-            if len(password) < 7 :
-               print("Password is less than 7 characters")
-
-            else:
-               # if password is greater than or equal to 7 characters
+   error = True
+   while error:
+      password_choice = input("Do you wish to change this password? Type 'Yes' or 'No' only: ")
+      if password_choice.lower() != "no" and password_choice.lower() != "yes":
+         print("You entered an invalid option")
+      else:
+         while password_use:
+            if password_choice.lower() == "no":
+               # save password if user does not wish to change generated password
                each_user["password"] = password
-               print("Your password has been updated")
-               enter_password = False
-         password_use = False
+               print("Your details have been saved")
+               password_use = False
+
+
+            elif password_choice.lower() == "yes":
+               # setting new password
+               enter_password = True
+
+               while enter_password:
+                  password = input("Enter a new password not less than 7 characters: ")
+                  # when password is less than 7 characters
+                  if len(password) < 7 :
+                     print("Password is less than 7 characters")
+
+                  else:
+                     # if password is greater than or equal to 7 characters
+                     each_user["password"] = password
+                     print("Your password has been updated")
+                     enter_password = False
+               password_use = False
+            error = False
    database.append(each_user)
 
    
